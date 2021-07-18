0459e18f0ad3f9ec6092e5bafa2a14bee7808a3c
Diana Koeswanto <d.koeswanto@gmail.com>
2021-07-18 16:07:33 +1000

    Added components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import About from './components/About';
+import Contacts from './components/Contacts';
+import Footer from './components/Footer';
+import Header from "./components/Header"
+import Portfolio from './components/Portfolio';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return(
+        <Router>
+            <Header />
+            <div className="content">
+                <Route exact path="/">
+                    <About />
+                </Route>
+                <Route path="/portfolio">
+                    <Portfolio />
+                </Route>
+                <Route path="/contacts">
+                    <Contacts />
+                </Route>
+            </div>
+            
+            <Footer />
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css'
+import Navbar from './Navbar';
+
+const Header = () => {
+    return(
+        <header className="header">
+            <Navbar />
+        </header>
+    )
+}
+
+export default Header;
